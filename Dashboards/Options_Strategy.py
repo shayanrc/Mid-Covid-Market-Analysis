ca7a6b000c249eb54f37061d77bdd090a5f13a9c
ShayanRC <shayan.roychoudhury@gmail.com>
2021-12-20 13:17:29 +0530

    update option strategy dashboard

--- Dashboards/Options_Strategy.py
+++ Dashboards/Options_Strategy.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+
 import seaborn as sns
 
 import streamlit as st
@@ -13,12 +14,18 @@ import nsepy
 import requests
 import json
 
+import plotly.express as px
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+
+
 st.set_page_config(
    page_title="Optimal Options",
    initial_sidebar_state="expanded",
 )
 DATA_PATH = os.path.join(os.path.dirname(os.path.dirname(__file__)),'datastore/processed')
-DATA_ROOT = 'QmXuhP4cHJfcuHpK6PdPwbzUHQo66W5vNNjHDfqTuTWhnR'
+DATA_ROOT = 'QmVmo99nn2wMjShNGi5CDPEqzRWtYxWRrjD5XTsfQiCZD3'
+# DATA_ROOT = 'QmYcxeeDVJzaHGwBxJNnZ66hbr9rV8BYMbmBHZxsUfWabH'
 
 def load_ohlcv_files(csv_folder):
     csv_files = glob.glob(csv_folder+'/*.csv',)
@@ -167,7 +174,7 @@ def evaluate_postions(options_data, underlying_period_delta, underlying_price, c
     
     return ce_df, pe_df
 
-def generate_pairs(pe_positions, ce_positions, brokerage=20):
+def generate_pairs(pe_positions, ce_positions, fitler_sigma=True, brokerage=20):
     
     # Convert to dict for easier iteration
     pe_positions = pe_positions.to_dict(orient='records')
@@ -177,15 +184,15 @@ def generate_pairs(pe_positions, ce_positions, brokerage=20):
 
     for pe_position in pe_positions:
 
-        if pe_position['sigma_returns'] <= 0:
+        if pe_position['sigma_returns'] <= 0 & fitler_sigma:
             continue
         for ce_position in ce_positions:
-            if ce_position['sigma_returns'] <= 0:
+            if ce_position['sigma_returns'] <= 0 & fitler_sigma:
                 continue
 
             pair_cost = pe_position['position_cost'] + ce_position['position_cost']
             if pair_cost < capital:
-                position_pair = {'cost': pair_cost}
+                position_pair = {'cost': pair_cost+brokerage*4}
                 position_pair['pe_price'] = pe_position['Close']
                 position_pair['ce_price'] = ce_position['Close']
                 position_pair['capital_not_breakeven_probability'] = (1-pe_position['probability_breakeven'])*(1-ce_position['probability_breakeven'])
@@ -202,7 +209,7 @@ def generate_pairs(pe_positions, ce_positions, brokerage=20):
 
     position_pairs = pd.DataFrame(position_pairs)
 
-    position_pairs['capital_breakeven_probability'] = 1-position_pairs['capital_not_breakeven_probability']
+    # position_pairs['capital_breakeven_probability'] = 1-position_pairs['capital_not_breakeven_probability']
     return position_pairs
 
 
@@ -231,7 +238,13 @@ def evaluate_pairs(position_pairs, underlying_close, delta_df, lot_size=50, expe
     return position_pairs
 
 @st.cache(allow_output_mutation=True, ttl=100)
-def load_options_data(underlying):
+def load_options_data(underlying, is_index=True):
+    
+    if is_index:
+        nse_url = 'https://www.nseindia.com/api/option-chain-indices'
+    else:
+        nse_url = 'https://www.nseindia.com/api/option-chain-equities'
+
     # Load Options Data
     # Get a copy of the default headers that requests would use
     headers = requests.utils.default_headers()
@@ -247,9 +260,10 @@ def load_options_data(underlying):
     resp = requests.get('https://www.nseindia.com/', headers=headers)
     cookies = resp.cookies.get_dict()
     
-    resp = requests.get('https://www.nseindia.com/api/option-chain-indices', params={'symbol':underlying}, headers=headers, cookies=cookies)
+    resp = requests.get(nse_url, params={'symbol':underlying}, headers=headers, cookies=cookies)
     # Load into a dataframe
     resp_json = json.loads(resp.text)
+    # print(resp_json)
     option_df = pd.DataFrame([record for record in map(flatten, resp_json['records']['data'])])
     option_df = option_df.rename({'underlying':'Symbol',
                                 'expiryDate': 'Expiry',
@@ -267,26 +281,38 @@ def load_options_data(underlying):
     expiries = resp_json['records']['expiryDates']
     return option_df, expiries
 
-PROVIDER_URL = 'https://ipfs.infura.io/ipfs/'
+# PROVIDER_URL = 'https://ipfs.infura.io/ipfs/'
+PROVIDER_URL = 'https://cloudflare-ipfs.com/ipfs/'
 
+@st.cache(allow_output_mutation=True, ttl=3600*12, persist=True)
 def load_underlying(underlying, data_root=DATA_ROOT):
     
     data_dict = requests.get(PROVIDER_URL+data_root).json()
-    data_hash = data_dict[underlying]
-
-     
-    underlying_df = pd.read_json(PROVIDER_URL+data_hash)
+    if underlying not in data_dict.keys():
+        underlying_df = nsepy.get_history(underlying, dt.datetime.today()-dt.timedelta(days=365*3), dt.datetime.today())
         
-    underlying_df.loc[:, 'Date'] = pd.to_datetime(underlying_df['Date'])
-    underlying_df = underlying_df.set_index('Date')
+    else:
+        data_hash = data_dict[underlying]        
+        underlying_df = pd.read_json(PROVIDER_URL+data_hash)
+        # print(underlying_df.head())
+        underlying_df.loc[:, 'Date'] = pd.to_datetime(underlying_df['Date'])
+        underlying_df = underlying_df.set_index('Date')
 
     return underlying_df
     
 
 st.title('Options Strategy Probability Optimization')
 
-instruments  = os.listdir(DATA_PATH)
-instruments = [x for x in instruments if not x.startswith('.')]
+# instruments  = ['NIFTY', 'BANKNIFTY', 'DLF']
+indices = ['NIFTY', 'BANKNIFTY']
+# instruments = [x for x in instruments if not x.startswith('.')]
+
+with open('/mnt/d/Workspace/Mid-Covid-Market-Analysis/datastore/nse.dataroot', mode='r') as f:
+    # data_dict = requests.get(PROVIDER_URL+f.readline()).json()
+    # data_hash = data_dict['NIFTY']
+    data_root = f.readline()
+    data_dict = requests.get(PROVIDER_URL+data_root).json()
+    instruments = list(data_dict.keys())
 
 sidebar_form = st.sidebar.form(key='sidebar_form')
 underlying = sidebar_form.selectbox('Underlying', instruments)
@@ -299,7 +325,14 @@ st.header('%s' % underlying)
 # underlying_df = load_ohlcv_files(ohlc_path)
 # underlying_df = pd.read_csv(ohlc_path, parse_dates=['Date'], index_col=['Date'])
 # underlying_df = pd.read_json('https://bafybeieoritg3qozjxewiamkin2ded7vri6j55w33kpljfvhciul5njsku.ipfs.infura-ipfs.io/')
-underlying_df = load_underlying(underlying)
+# underlying_df = load_underlying(underlying)
+
+    # data_dict = requests.get(PROVIDER_URL+f.readline()).json()
+    # print(data_dict.keys())
+    # underlying_df = pd.read_json(PROVIDER_URL+data_hash)
+
+
+underlying_df = load_underlying(underlying, data_root=data_root)
 underlying_df
 with st.expander('Historical data details') as underlying_upload_expander:
     st.write('Earliest Historical data available:')
@@ -332,13 +365,16 @@ today = dt.date.today()
 # And keep only future expiris
 # expiries = [expiry for expiry in expiries if expiry>today]
 
-
-option_df, expiries = load_options_data(underlying)
+is_index = underlying in indices
+option_df, expiries = load_options_data(underlying, is_index=is_index)
 # if len(expiries)==0:
 #     expiries = [dt.date.today()]
 # trade_days_left = np.busday_count(today, expiries)
 last_close = option_df['Underlying'].values[0]
-last_close = st.number_input('Last Close', min_value=round_to_base(last_close*0.9, base=100), max_value=round_to_base(last_close*1.1, base=100), value=int(last_close), step=100)
+if last_close == 0:
+    last_close = underlying_df.Close.iloc[-1]
+# st.write(last_close)
+last_close = sidebar_form.number_input('Last Close', min_value=round_to_base(last_close*0.9, base=10), max_value=round_to_base(last_close*1.1, base=10), value=int(last_close), step=10)
 
 expiry = sidebar_form.selectbox('Choose Expiry', expiries)
 # calc_trade_days_left = trade_days_left[expiries.index(expiry)]
@@ -349,43 +385,98 @@ calc_trade_days_left = np.busday_count(today, dt.datetime.strptime(expiry, '%d-%
 # st.write('Trade Days Left to Expiry:' , calc_trade_days_left)
 st.markdown('## Historical Change Analysis')   
 
-trade_days_left = st.slider('Adjust Trade Days Left to Expiry (in case of holidays)', 1, 32, int(calc_trade_days_left))
+trade_days_left = sidebar_form.slider('Adjust Trade Days Left to Expiry', 1, 32, int(calc_trade_days_left))
 
 underlying_period_delta = calculate_period_delta(underlying_df, trade_days_left)
 
 
-fig = plt.figure(figsize=(32,28))
-ax = fig.add_subplot(311)
-ax.plot(underlying_df.Close)
-ax.grid()
-ax.set_title('%s Daily Close'% underlying)
-# fig = plt.figure(figsize=(23,5))
-ax = fig.add_subplot(312)
-ax.set_ylim(bottom=-0.1, top=0.1)
-# ax.bar(data_df.index, (data_df.Open - data_df.Close)/data_df.Open)
-ax.bar(underlying_period_delta.index, underlying_period_delta['delta'])
-ax.set_title('%s Period Change' % underlying)
-ax.grid()
-ax = fig.add_subplot(313)
-
-ax.set_ylim(bottom=0, top=0.1)
-# ax.plot(data_df.Close.rolling(trade_days_left).apply(lambda x: (x[-1]-x.mean())/x.std()))
+# fig = plt.figure(figsize=(32,28))
+# ax = fig.add_subplot(311)
+# ax.plot(underlying_df.Close)
 # ax.grid()
-# ax.set_title('Stationarized')
-ax.bar(underlying_period_delta.index, 
-       (underlying_period_delta['delta_max']-underlying_period_delta['delta_min']), label="Volatility")#-abs(underlying_period_delta['delta']))
+# ax.set_title('%s Daily Close'% underlying)
+# # fig = plt.figure(figsize=(23,5))
+# ax = fig.add_subplot(312)
+# ax.set_ylim(bottom=-0.1, top=0.1)
+# # ax.bar(data_df.index, (data_df.Open - data_df.Close)/data_df.Open)
+# ax.bar(underlying_period_delta.index, underlying_period_delta['delta'])
+# ax.set_title('%s Period Change' % underlying)
+# ax.grid()
+# ax = fig.add_subplot(313)
 
-ax.bar(underlying_period_delta.index, 
-       abs(underlying_period_delta['delta']), label="Trend")#-abs(underlying_period_delta['delta']))
+# ax.set_ylim(bottom=0, top=0.1)
+# # ax.plot(data_df.Close.rolling(trade_days_left).apply(lambda x: (x[-1]-x.mean())/x.std()))
+# # ax.grid()
+# # ax.set_title('Stationarized')
+# ax.bar(underlying_period_delta.index, 
+#        (underlying_period_delta['delta_max']-underlying_period_delta['delta_min']), label="Volatility")#-abs(underlying_period_delta['delta']))
 
-# ax.plot(abs(underlying_period_delta['delta'].rolling(5).mean()), '.',color='green')
-# ax.plot(abs(underlying_period_delta['delta'].rolling(3).mean()), color='lightgreen')
-ax.grid()
-ax.set_title('Volatility')
-ax.legend()
+# ax.bar(underlying_period_delta.index, 
+#        abs(underlying_period_delta['delta']), label="Trend")#-abs(underlying_period_delta['delta']))
 
-st.pyplot(fig)
+# # ax.plot(abs(underlying_period_delta['delta'].rolling(5).mean()), '.',color='green')
+# # ax.plot(abs(underlying_period_delta['delta'].rolling(3).mean()), color='lightgreen')
+# ax.grid()
+# ax.set_title('Volatility')
+# ax.legend()
 
+# st.pyplot(fig)
+
+# df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/finance-charts-apple.csv')
+
+# fig = px.line(underlying_df.reset_index(), x='Date', y='Close', title=underlying)
+
+# fig.update_xaxes(rangeslider_visible=True)
+# fig.update_layout(
+#     margin=dict(l=10, r=0, t=25, b=0),
+#     # paper_bgcolor="LightSteelBlue",
+# )
+fig = make_subplots(rows=3, cols=1, 
+                    shared_xaxes=True, 
+                    vertical_spacing=0.1, 
+                    subplot_titles = ['Underlying', 'Trend', 'Computed Volatility'])
+
+fig.append_trace(go.Candlestick(x=underlying_df.index, 
+                                open=underlying_df['Open'], 
+                                high=underlying_df['High'], 
+                                low=underlying_df['Low'], 
+                                close=underlying_df['Close']),
+                 row=1, col=1)
+fig.append_trace(go.Scatter(x=underlying_df.index, 
+                            y=underlying_period_delta['delta'], 
+                            fill='tozeroy',
+                            mode='none'),
+                 row=2, col=1)
+fig.append_trace(go.Bar(x=underlying_df.index, 
+                        y=underlying_period_delta['delta_max']-underlying_period_delta['delta_min']),
+                 row=3, col=1)
+fig.append_trace(go.Bar(x=underlying_df.index, 
+                        y=abs(underlying_period_delta['delta'])),
+                 row=3, col=1)
+# fig = go.Figure(data=[go.Candlestick(x=underlying_df.index,
+#                 open=underlying_df['Open'], high=underlying_df['High'],
+#                 low=underlying_df['Low'], close=underlying_df['Close'])
+#                       ])
+# fig.update_xaxes(rangeslider_visible=True)
+fig.update_layout(
+    autosize=False,
+    width=800,
+    height=800,
+    title='%s'%underlying,
+    xaxis_rangeslider_visible=False,
+    showlegend = False,
+    # yaxis_title='Daily',
+    # shapes = [dict(
+    #     x0='2016-12-09', x1='2016-12-09', y0=0, y1=1, xref='x', yref='paper',
+    #     line_width=2)],
+    # annotations=[dict(
+    #     x='2016-12-09', y=0.05, xref='x', yref='paper',
+    #     showarrow=False, xanchor='left', text='Increase Period Begins')]
+    margin=dict(l=10, r=0, t=25, b=0)
+)
+fig.update_xaxes(rangeslider= {'visible':True}, row=3, col=1)
+
+st.plotly_chart(fig)
 
 mean_delta = underlying_period_delta['delta'].mean()
 std_delta = underlying_period_delta['delta'].std()
@@ -490,7 +581,6 @@ st.header('Options Analysis')
 
 # Filter for latest data
 # expiry = st.selectbox('Choose Expiry', options_df['Expiry'].unique())
-
 # options_df['Expiry'] = pd.to_datetime(options_df['Expiry'])
 # options_df = options_df[options_df['Expiry']==expiry]
 option_df = option_df[option_df['Expiry']==expiry]
@@ -507,7 +597,9 @@ option_df['Expiry'] = pd.to_datetime(option_df['Expiry'])
 # options_df = options_df[options_df.index.max()==options_df.index]
 # Get rid of contracts which are not actively traded
 
-option_df = option_df[option_df['Number of Contracts']>1000]
+# option_df = option_df[option_df['Number of Contracts']>1000]
+# option_df = option_df[option_df['Change']!=0]
+option_df = option_df[option_df['Change']!=0]
 
 with st.expander('Preview %s Options Data'% underlying) as  ohlc_preview:
     # Display Head and Tail
@@ -517,7 +609,7 @@ with st.expander('Preview %s Options Data'% underlying) as  ohlc_preview:
 
 capital = sidebar_form.number_input('Capital', min_value=1000, max_value=50000, value=15000, step=500)
 lot_size = sidebar_form.number_input('Lot Size', min_value=1, max_value=10000, value=50, step=10)
-expected_rate = sidebar_form.slider('Minimumum Expected Returns (%)', min_value=0.5, max_value=25.0, value=2.0, step=0.5)/100
+expected_rate = sidebar_form.slider('Slippage (%)', min_value=0.5, max_value=25.0, value=2.0, step=0.5)/100
 
 
 sidebar_form.form_submit_button("Scan Opportunities")
@@ -550,17 +642,18 @@ sidebar_form.form_submit_button("Scan Opportunities")
 #                                                        'cost':pair_cost})
 
 
-if 'current_positions' in st.session_state:
-    current_positions_df = pd.DataFrame(st.session_state.current_positions)
-    st.dataframe(current_positions_df)
-
 
 ce_df, pe_df = evaluate_postions(options_data=option_df, underlying_period_delta=underlying_period_delta, underlying_price=last_close, capital=capital, expected_rate=expected_rate, lot_size=lot_size)
 
 period_volatility = (underlying_period_delta['delta_max'] - underlying_period_delta['delta_min']).iloc[-1]
 prev_period_volatility = (underlying_period_delta['delta_max'] - underlying_period_delta['delta_min']).iloc[-2]
-ce_df = ce_df[ce_df['change_breakeven']<period_volatility]
-pe_df = pe_df[pe_df['change_breakeven'].abs()<period_volatility]
+col1, col2 = st.columns(2)
+filter_volatility = col1.checkbox('Filter Volatility', value=True)
+filter_sigma = col2.checkbox('Filter Sigma', value=True)
+
+if filter_volatility:
+    ce_df = ce_df[ce_df['change_breakeven']<period_volatility]
+    pe_df = pe_df[pe_df['change_breakeven'].abs()<period_volatility]
 
 
 with st.expander('Put Positions Analysis'):
@@ -570,26 +663,70 @@ with st.expander('Call Positions Analysis'):
     st.table(ce_df[['Strike Price', 'Close', 'probability_breakeven', 'probability_itm']].sort_values('probability_breakeven', ascending=False, ignore_index=True))
 
 
-
-position_pairs = generate_pairs(pe_df, ce_df)
+position_pairs = generate_pairs(pe_df, ce_df, fitler_sigma=filter_sigma)
 eval_pairs = evaluate_pairs(position_pairs, last_close, underlying_period_delta, lot_size=lot_size, expected_rate=expected_rate)
 
 
 
 # "Volatility %s %" % period_volatility
 # st.text("Trend %f %"% (underlying_period_delta['delta'].iloc[-1]))
-col1, col2 = st.columns(2)
+col1, col2, col3 = st.columns(3)
 
-col1.metric(label="Volatility", 
+col1.metric(label="Underlying", 
+            value="{:.2f}".format(last_close), 
+            delta="{:.2f} %".format(100*(last_close - underlying_df.Close.iloc[-1])/underlying_df.Close.iloc[-1]))
+col2.metric(label="Volatility", 
             value="{:.2f} %".format(100*period_volatility), 
             delta="{:.2f} %".format(100*(period_volatility-prev_period_volatility)))
 
-col2.metric(label="Trend", 
+col3.metric(label="Trend", 
             value="{:.2f} %".format(100*underlying_period_delta['delta'].iloc[-1]), 
             delta="{:.2f} %".format(100*underlying_period_delta['delta'].iloc[-1]-underlying_period_delta['delta'].iloc[-2]))
 st.header('Recomended Pairs')
 # max(eval_pairs['ce_strike_price']-last_close, last_close-eval_pairs['pe_strike_price'])
-dsp_cols = ['cost', 'pe_strike_price', 'ce_strike_price', 'pe_price', 'ce_price', 'target', 'pair_otm_probability', 'pair_not_breakeven_probability', 'spread']
+dsp_cols = ['cost', 'ce_strike_price', 'pe_strike_price', 'ce_price', 'pe_price', 'target', 'pair_otm_probability', 'pair_not_breakeven_probability', 'spread']
 # last_close - eval_pairs['pe_strike_price'], eval_pairs['ce_strike_price'] - last_close
+
+def highlight_pairs(x):
+    if x['target']>100:
+        return 'background-color: red'
+    # return ['background-color: red' if v == x.max() else '' for v in x]
+
+# eval_pairs
 # dsp_pairs = eval_pairs[(period_volatility*last_close)>(eval_pairs['target']+eval_pairs['spread'])]
-st.dataframe(eval_pairs[dsp_cols])
+# dsp_pairs = eval_pairs[period_volatility>(eval_pairs['ce_breakeven_change']/last_close)]
+# dsp_pairs = dsp_pairs[period_volatility>abs(dsp_pairs['pe_breakeven_change']/last_close)]
+dsp_pairs = eval_pairs[dsp_cols]
+
+fig = px.scatter(dsp_pairs, x='pair_otm_probability', y='pair_not_breakeven_probability', size='spread', color='cost', hover_data=['ce_strike_price', 'pe_strike_price'])
+x_min = dsp_pairs['pair_otm_probability'].min()
+y_min = dsp_pairs['pair_not_breakeven_probability'].min()
+# fig.add_annotation(
+#             x=x_min,
+#             y=0.5,
+#             text='Long Opportunities',
+#             # xref="paper",
+#             # yref="paper",
+#             align="right",
+#             showarrow=False,
+#             font_size=10
+# )
+# fig.add_shape(type="rect",
+#     # xref="paper", yref="paper",
+#     x0=x_min, y0=y_min,
+#     x1=0.5, y1=0.5,
+#     line=dict(
+#         color="Blue",
+#         width=1,
+#     ),
+#     opacity=0.25,
+#     fillcolor="RoyalBlue",
+# )
+fig.add_vline(x=0.5, line_dash="dot",
+              annotation_text="P(OTM @ Expiry) = 0.5", 
+              annotation_position="bottom right")
+fig.add_hrect(y0=y_min*0.9, y1=0.5, 
+              annotation_text="Long", annotation_position="top left",
+              fillcolor="green", opacity=0.25, line_width=0)
+st.plotly_chart(fig)
+st.dataframe(dsp_pairs)
